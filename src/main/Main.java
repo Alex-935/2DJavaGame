c396de63ed0d7a41e88ab383a8a604b150764fed
Alex <69348601+Alex-935@users.noreply.github.com>
2025-10-24 12:08:11 +0100

    Init Commit

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("2D Game");
+    }
+}
