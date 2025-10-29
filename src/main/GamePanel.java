702259af1a6f9dfb21cf3caa261bceb5ca19cd50
Alex-935 <alex.potter3@hotmail.co.uk>
2025-10-29 18:11:02 +0000

    Act on Key Input

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,8 +15,17 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; //768 px
     final int screenHeight = tileSize * maxScreenRow; //576 px
 
+    // FPS = 60
+    int FPS = 60;
+
+
     KeyHandler keyH = new KeyHandler();
     Thread gameThread; //can be started and stopped, useful for rerunning a set of actions.
+
+    // Set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -38,6 +47,9 @@ public class GamePanel extends JPanel implements Runnable {
         while (gameThread != null) {
             //System.out.println("Game Loop Running");
 
+            long currentTime = System.nanoTime();
+            //long currentTime = System.milliTime(); is also an option but nano is more precise
+
             // 1. UPDATE: update information such as character position
             update();
             // 2. DRAW: draw the screen with updated information
@@ -46,7 +58,18 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -55,7 +78,7 @@ public class GamePanel extends JPanel implements Runnable {
                                         // over geometry, coordinate transformations, color management and text layout
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();// dispose of graphics content and release system resources it is using.
     }
 
